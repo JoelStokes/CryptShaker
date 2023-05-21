668670fa92b70e24b4e60b6b3d89900c6a03bd88
Joel Stokes <joel.stokes.e@gmail.com>
2023-05-20 20:29:40 -0400

    WebGL Build / Music Fix

--- Assets/Scripts/PersistentAudio.cs
+++ Assets/Scripts/PersistentAudio.cs
@@ -5,23 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class PersistentAudio : MonoBehaviour
 {
-    public AudioSource audioSource;
-    private bool audioBegin = false;
-
     private void Awake() {
         GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
         
-        if (!audioBegin) {
-            if (objs.Length > 1)
-            {
-                Destroy(gameObject);
-            }
-
-            audioSource.Play ();
-            audioBegin = true;
-
-            DontDestroyOnLoad(gameObject);
+        if (objs.Length > 1)
+        {
+            Destroy(gameObject);
         }
+
+        DontDestroyOnLoad(gameObject);
+
+        if (SceneManager.GetActiveScene().name == "Title"){
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
